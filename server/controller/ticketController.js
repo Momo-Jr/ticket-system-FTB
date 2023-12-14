efc23056d925f97e651affb0f4a3e66ee3caa313
Mohamed Mohsen <mohamedmohsennashat@gmail.com>
2023-12-14 23:10:11 +0200

    update and delete tickets added

--- server/controller/ticketController.js
+++ server/controller/ticketController.js
@@ -12,8 +12,8 @@ const getTickets = expressAsyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("User Not Found");
   }
-  const ticket = await Ticket.find({ user: req.user.id });
-  res.status(200).json(ticket);
+  const tickets = await Ticket.find({ user: req.user.id });
+  res.status(200).json(tickets);
 });
 
 // @desc Create user tickets
@@ -41,7 +41,84 @@ const createTickets = expressAsyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
+// @desc Get user ticket
+// @route GET /api/tickets/:id
+// access private
+const getTicket = expressAsyncHandler(async (req, res) => {
+  // Get user by ID
+  const user = User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User Not Found");
+  }
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not Found");
+  }
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+  res.status(200).json(ticket);
+});
+
+// @desc DELETE user ticket
+// @route DELETE /api/tickets/:id
+// access private
+const deleteTicket = expressAsyncHandler(async (req, res) => {
+  // Get user by ID
+  const user = User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User Not Found");
+  }
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not Found");
+  }
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+  await ticket.remove();
+  res.status(200).json({ success: true });
+});
+
+// @desc update user ticket
+// @route PUT /api/tickets/:id
+// access private
+const updateTicket = expressAsyncHandler(async (req, res) => {
+  // Get user by ID
+  const user = User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User Not Found");
+  }
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not Found");
+  }
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedTicket);
+});
+
 module.exports = {
   getTickets,
   createTickets,
+  getTicket,
+  updateTicket,
+  deleteTicket,
 };
--- server/routes/ticketRoute.js
+++ server/routes/ticketRoute.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const { protect } = require("../middleware/authorizationMiddleware");
-const { getTickets, createTickets } = require("../controller/ticketController");
+const {
+  getTickets,
+  createTickets,
+  getTicket,
+  deleteTicket,
+  updateTicket,
+} = require("../controller/ticketController");
 const router = express.Router();
 
 router.route("/").get(protect, getTickets).post(protect, createTickets);
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
